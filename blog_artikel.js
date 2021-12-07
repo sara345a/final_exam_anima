57a9d3d2f0959ad509a686043306f43cf2ef019e
siff0077 <61138080+siff0077@users.noreply.github.com>
2021-12-07 10:12:07 +0100

    starting on testamenter-page, contact-page mostly done, still working on blog- and underskriv-page

--- blog_artikel.js
+++ blog_artikel.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", hentData);
 
-let template = document.querySelector("template");
 
 //Henter json data og starter forsiden (async for at få loadet json før man går videre)
 async function hentData() {
@@ -11,11 +10,11 @@ async function hentData() {
     const json = await respons.json();
 
     //vis forsiden
-    vis(json);
+    visSplash(json);
 }
 
 //Indsætter data fra wordpress på rette pladser på forsiden
-async function vis(data) {
+async function visSplash(data) {
     console.log("vis");
 
     document.querySelector("#splash").style.backgroundImage = "url(" + data.splash1.guid + ")";
@@ -25,26 +24,18 @@ async function vis(data) {
 
 
 
-    const postlink = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/blog_posts"
+    const postlink = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/blog_posts/71"
     const responspost = await fetch(postlink);
     const jsonpost = await responspost.json();
-    let filtrerede;
-    console.log("Det virker!!!");
 
-    console.log(jsonpost);
-
-    //filtrer efter kategori og vis
-    jsonpost.forEach((post) => {
-        const klon = template.cloneNode(true).content;
-        klon.querySelector(".post_h2").textContent = post.title.rendered;
-        klon.querySelector(".post_excerpt").innerHTML = post.excerpt.rendered;
-        klon.querySelector(".post_image").src = post.blog_posts.guid;
-        document.querySelector("#blogcontent").appendChild(klon);
-        console.log("appendChild");
-
-    })
-
-
+    visArticle(jsonpost)
 }
 
 
+    function visArticle(jsonpost) {
+
+        document.querySelector("#article_content").textContent = jsonpost.content.rendered;
+
+
+
+}
