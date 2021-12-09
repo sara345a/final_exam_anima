50be39d7ae064969f8e9eee1f9612aa478eaedd2
Sara Nielsen <sara345a@stud.kea.dk>
2021-12-09 20:19:21 +0100

    frontpage and about us

---
+++ componenter/nav_plus_splash/splash.js
@@ -0,0 +1,125 @@
+/* */ //////////////////////////BRUGER MENU/////////////////////////////////////////// */
+window.addEventListener("DOMContentLoaded", initPage);
+function initPage() {
+  const menu = document.querySelector("#burger_menu");
+  const menuLinks = document.querySelector(".nav_container");
+
+  menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+  });
+}
+
+///////////////////////////SLIDESHOW///////////////////////////////////////////
+var slideIndex = 0;
+showSlides();
+var slides, dots;
+var i;
+function showSlides() {
+  slides = document.querySelectorAll(".mySlides");
+  dots = document.querySelectorAll(".dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5500);
+}
+
+function plusSlides(position) {
+  slideIndex += position;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function currentSlide(index) {
+  if (index > slides.length) {
+    index = 1;
+  } else if (index < 1) {
+    index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[index - 1].style.display = "block";
+  dots[index - 1].className += " active";
+}
+
+///////////////////////////STICKY NAVBAR WHEN SCROLL///////////////////////////////////////////
+window.onscroll = function () {
+  makeNavbarSticky();
+};
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeNavbarSticky() {
+  if (window.scrollY > 50) {
+    navbar.classList.add("sticky");
+    navbar.classList.add("background_color");
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.remove("background_color");
+  }
+}
+
+///////////////////////////HENT DATAL///////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", hentData);
+
+//Henter json data og starter forsiden (async for at få loadet json før man går videre)
+
+async function hentData() {
+  console.log("hentData");
+
+  const link = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/pages/2/";
+
+  const respons = await fetch(link);
+
+  const json = await respons.json();
+
+  //vis forsiden
+  vis(json);
+}
+
+async function vis(data) {
+  console.log("vis");
+  /* document.querySelector("#splash").style.backgroundImage = "url(" + data.splashbillede.guid + ")"; */
+  // document.querySelector(".p").textContent = data.content.rendered;
+  // document.querySelector("#blogindhold").innerHTML = data.content.rendered;
+
+  /* document.querySelector(".picture_one").src = data.kampagne_splash1.guid;
+document.querySelector(".picture_two").src = data.kampagne_splash2.guid;
+document.querySelector(".picture_three").src = data.kampagne_splash3.guid; */
+
+  document.querySelector(".picture_one").style.backgroundImage =
+    "url(" + data.splash1.guid + ")";
+  document.querySelector(".picture_two").style.backgroundImage =
+    "url(" + data.splash2.guid + ")";
+  document.querySelector(".picture_three").style.backgroundImage =
+    "url(" + data.splash3.guid + ")";
+}
