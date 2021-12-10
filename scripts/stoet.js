31d01ed256b9dcd25496773247c0023b075ea2c5
siff0077 <61138080+siff0077@users.noreply.github.com>
2021-12-10 14:50:33 +0100

    small changes

--- scripts/stoet.js
+++ scripts/stoet.js
@@ -1,23 +1,60 @@
+/* */ //////////////////////////BRUGER MENU/////////////////////////////////////////// */
+window.addEventListener("DOMContentLoaded", initPage);
+function initPage() {
+  const menu = document.querySelector("#burger_menu");
+  const menuLinks = document.querySelector(".nav_container");
+
+  menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+  });
+}
+///////////////////////////STICKY NAVBAR WHEN SCROLL///////////////////////////////////////////
+window.onscroll = function () {
+  makeNavbarSticky();
+};
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeNavbarSticky() {
+  if (window.scrollY > 50) {
+    navbar.classList.add("sticky");
+    navbar.classList.add("background_color");
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.remove("background_color");
+  }
+}
+
+///////////////////////////HENT DATAL///////////////////////////////////////////
+
 document.addEventListener("DOMContentLoaded", hentData);
 
 //Henter json data og starter forsiden (async for at få loadet json før man går videre)
+
 async function hentData() {
-    console.log("hentData");
+  console.log("hentData");
 
-    const link = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/pages/98/"
-    const respons = await fetch(link);
-    const json = await respons.json();
+  const link = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/pages/98/";
 
-    //vis forsiden
-    vis(json);
+  const respons = await fetch(link);
+
+  const json = await respons.json();
+
+  //vis forsiden
+  vis(json);
 }
 
-//Indsætter data fra wordpress på rette pladser på forsiden
 async function vis(data) {
-    console.log("vis");
+  console.log("vis");
 
-    document.querySelector("#splash").style.backgroundImage = "url(" + data.splash1.guid + ")";
-    document.querySelector("#title").textContent = data.title.rendered;
+  document.querySelector(".picture_one").style.backgroundImage =
+    "url(" + data.splash1.guid + ")";
+
     document.querySelector("#icon_1 img").src = data.ikon1.guid;
     document.querySelector("#icon_1 h4").textContent = data.ikon_overskrift1;
     document.querySelector("#icon_1 p").textContent = data.ikon_tekst1;
@@ -31,6 +68,6 @@ async function vis(data) {
     document.querySelector(".textbox h3").textContent = data.ikon_overskrift1;
     document.querySelector(".textbox p").textContent = data.tekstfelt1;
     document.querySelector("#bottom_column img").src = data.indholdsbillede1.guid;
-
-    
 }
+
+
