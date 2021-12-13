67413be5593f0652f9b4f6da25028d0fa07d5434
simo86e6 <61137952+simo86e6@users.noreply.github.com>
2021-12-13 14:14:44 +0100

    endelig filtrering

--- scripts/underskriv.js
+++ scripts/underskriv.js
@@ -41,6 +41,8 @@ let jsonpost;
 let categoryJson;
 let json;
 
+const categoryIds = [];
+
 //Henter json data og starter forsiden (async for at få loadet json før man går videre)
 async function hentData() {
     console.log("hentData");
@@ -49,7 +51,7 @@ async function hentData() {
     const respons = await fetch(link);
     json = await respons.json();
 
-    const postlink = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/underskriv_posts"
+    const postlink = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/underskriv_post"
     const responspost = await fetch(postlink);
     jsonpost = await responspost.json();
 
@@ -58,33 +60,33 @@ async function hentData() {
     categoryJson = await categoryRespons.json();
 
     //vis forsiden
-    vis(json);
+    vis();
     addButtons();
 
 }
 
 //Indsætter data fra wordpress på rette pladser på forsiden
-async function vis(data) {
+async function vis() {
     console.log("vis");
 
     document.querySelector(".picture_one").style.backgroundImage = "url(" + json.splash1.guid + ")";
-    document.querySelector(".text h1").textContent = data.splash_overskrift1;
-    document.querySelector(".text p").textContent = data.splash_tekst1;
-   
-
-    let filtrerede;
-    console.log("Det virker!!!");
-
-    console.log(jsonpost);
+    document.querySelector(".text h1").textContent = json.splash_overskrift1;
+    document.querySelector(".text p").textContent = json.splash_tekst1;
+  
 
     //filtrer efter kategori og vis
     jsonpost.forEach((post) => {
         const klon = template.cloneNode(true).content;
         klon.querySelector(".post_h2").textContent = post.title.rendered;
         klon.querySelector(".post_excerpt").innerHTML = post.excerpt.rendered;
-        klon.querySelector(".post_image").src = post.blog_posts.guid;
+        klon.querySelector(".post_image").src = post.underskriv_posts.guid;
         document.querySelector("#blogcontent").appendChild(klon);
         console.log("appendChild");
+
+        let ids = post.categories;
+        ids.forEach(id => {
+          categoryIds.push(id);
+        });
 
     })
 
@@ -108,7 +110,7 @@ function filterClicked(buttonClicked){
                 const klon = template.cloneNode(true).content;
             klon.querySelector(".post_h2").textContent = post.title.rendered;
             klon.querySelector(".post_excerpt").innerHTML = post.excerpt.rendered;
-            klon.querySelector(".post_image").src = post.blog_posts.guid;
+            klon.querySelector(".post_image").src = post.underskriv_posts.guid;
             document.querySelector("#blogcontent").appendChild(klon);
             console.log("appendChild");
     
@@ -122,23 +124,24 @@ function filterClicked(buttonClicked){
 
 async function addButtons(){
 
-    categoryJson.forEach((category) => {
-        if(category.name == "Ikke-kategoriseret"){
-            return;
-        }
-        const klon = categoryTemplate.cloneNode(true).content;
-        klon.querySelector(".filter_btn").innerHTML = category.name;
-        klon.querySelector(".filter_btn").val = category.id;
+  categoryJson.forEach((category) => {
 
-        document.querySelector("#button_box").appendChild(klon);
+    if(categoryIds.includes(category.id) || category.name == "Alle"){
+      const klon = categoryTemplate.cloneNode(true).content;
+      klon.querySelector(".filter_btn").innerHTML = category.name;
+      klon.querySelector(".filter_btn").val = category.id;
+
+      document.querySelector("#button_box").appendChild(klon);
+    }
+
+      
+  })
+
+  document.querySelectorAll('.filter_btn').forEach(btn => {
+      btn.addEventListener('click', filterClicked)
     })
 
-    document.querySelectorAll('.filter_btn').forEach(btn => {
-        btn.addEventListener('click', filterClicked)
-      })
-
 }
-
 
 
 
