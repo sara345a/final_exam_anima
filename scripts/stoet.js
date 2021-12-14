4a4f006982eeb53bc09e00dfd18f663953a1f339
Sara Nielsen <sara345a@stud.kea.dk>
2021-12-15 00:15:25 +0100

    støt page almost done

--- scripts/stoet.js
+++ scripts/stoet.js
@@ -54,22 +54,21 @@ async function vis(data) {
 
   document.querySelector(".picture_one").style.backgroundImage =
     "url(" + data.splash1.guid + ")";
-    document.querySelector(".text h1").textContent = data.splash_overskrift1;
-    document.querySelector(".text p").textContent = data.splash_tekst1;
+  document.querySelector(".text h1").textContent = data.splash_overskrift1;
+  document.querySelector(".text p").textContent = data.splash_tekst1;
 
-    document.querySelector("#icon_1 img").src = data.ikon1.guid;
-    document.querySelector("#icon_1 h4").textContent = data.ikon_overskrift1;
-    document.querySelector("#icon_1 p").textContent = data.ikon_tekst1;
-    document.querySelector("#icon_2 img").src = data.ikon2.guid;
-    document.querySelector("#icon_2 h4").textContent = data.ikon_overskrift2;
-    document.querySelector("#icon_2 p").textContent = data.ikon_tekst2;
-    document.querySelector("#icon_3 img").src = data.ikon3.guid;
-    document.querySelector("#icon_3 h4").textContent = data.ikon_overskrift3;
-    document.querySelector("#icon_3 p").textContent = data.ikon_tekst3;
+  document.querySelector("#icon_1 img").src = data.ikon1.guid;
+  document.querySelector("#icon_1 h4").textContent = data.ikon_overskrift1;
+  document.querySelector("#icon_1 p").textContent = data.ikon_tekst1;
+  document.querySelector("#icon_2 img").src = data.ikon2.guid;
+  document.querySelector("#icon_2 h4").textContent = data.ikon_overskrift2;
+  document.querySelector("#icon_2 p").textContent = data.ikon_tekst2;
+  document.querySelector("#icon_3 img").src = data.ikon3.guid;
+  document.querySelector("#icon_3 h4").textContent = data.ikon_overskrift3;
+  document.querySelector("#icon_3 p").textContent = data.ikon_tekst3;
 
-    document.querySelector(".textbox h3").textContent = data.overskrift1;
-    document.querySelector(".textbox p").textContent = data.tekstfelt1;
-    document.querySelector("#bottom_column img").src = data.indholdsbillede1.guid;
+  document.querySelector(".two-third_right h3").textContent = data.overskrift1;
+  document.querySelector(".two-third_right p").textContent = data.tekstfelt1;
+  document.querySelector(".two-third_left img").src =
+    data.indholdsbillede1.guid;
 }
-
-
