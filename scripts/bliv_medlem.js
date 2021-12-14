c1e2e31ccae6cf089ad2d98361aa78b158e31c0f
Sara Nielsen <sara345a@stud.kea.dk>
2021-12-15 00:44:53 +0100

    bliv medlem almost done

--- scripts/bliv_medlem.js
+++ scripts/bliv_medlem.js
@@ -32,43 +32,41 @@ function makeNavbarSticky() {
 
 ///////////////////////////HENT DATAL///////////////////////////////////////////
 
-
-
 document.addEventListener("DOMContentLoaded", hentData);
 
 //Henter json data og starter forsiden (async for at få loadet json før man går videre)
 async function hentData() {
-    console.log("hentData");
+  console.log("hentData");
 
-    const link = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/pages/141/"
-    const respons = await fetch(link);
-    const json = await respons.json();
+  const link = "https://jmotte.dk/eksamen-anima/wp-json/wp/v2/pages/141/";
+  const respons = await fetch(link);
+  const json = await respons.json();
 
-    //vis forsiden
-    vis(json);
+  //vis forsiden
+  vis(json);
 }
 
 //Indsætter data fra wordpress på rette pladser på forsiden
 async function vis(data) {
-    console.log("vis");
+  console.log("vis");
 
-    document.querySelector(".picture_one").style.backgroundImage = "url(" + data.splash1.guid + ")";
-    document.querySelector(".text h1").textContent = data.splash_overskrift1;
-    document.querySelector(".text p").textContent = data.splash_tekst1;
-  
-    document.querySelector("#icon_1 img").src = data.ikon1.guid;
-    document.querySelector("#icon_1 h4").textContent = data.ikon_overskrift1;
-    document.querySelector("#icon_1 p").textContent = data.ikon_tekst1;
-    document.querySelector("#icon_2 img").src = data.ikon2.guid;
-    document.querySelector("#icon_2 h4").textContent = data.ikon_overskrift2;
-    document.querySelector("#icon_2 p").textContent = data.ikon_tekst2;
-    document.querySelector("#icon_3 img").src = data.ikon3.guid;
-    document.querySelector("#icon_3 h4").textContent = data.ikon_overskrift3;
-    document.querySelector("#icon_3 p").textContent = data.ikon_tekst3;
+  document.querySelector(".picture_one").style.backgroundImage =
+    "url(" + data.splash1.guid + ")";
+  document.querySelector(".text h1").textContent = data.splash_overskrift1;
+  document.querySelector(".text p").textContent = data.splash_tekst1;
 
-    document.querySelector(".textbox h3").textContent = data.overskrift1;
-    document.querySelector(".textbox p").textContent = data.tekstfelt1;
-    document.querySelector("#bottom_column img").src = data.indholdsbillede1.guid;
+  document.querySelector("#icon_1 img").src = data.ikon1.guid;
+  document.querySelector("#icon_1 h4").textContent = data.ikon_overskrift1;
+  document.querySelector("#icon_1 p").textContent = data.ikon_tekst1;
+  document.querySelector("#icon_2 img").src = data.ikon2.guid;
+  document.querySelector("#icon_2 h4").textContent = data.ikon_overskrift2;
+  document.querySelector("#icon_2 p").textContent = data.ikon_tekst2;
+  document.querySelector("#icon_3 img").src = data.ikon3.guid;
+  document.querySelector("#icon_3 h4").textContent = data.ikon_overskrift3;
+  document.querySelector("#icon_3 p").textContent = data.ikon_tekst3;
 
-    
+  document.querySelector(".two-third_right h3").textContent = data.overskrift1;
+  document.querySelector(".two-third_right p").textContent = data.tekstfelt1;
+  document.querySelector(".two-third_left img").src =
+    data.indholdsbillede1.guid;
 }
